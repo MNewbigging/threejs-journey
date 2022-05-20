f8ed4c5437074748a5f6c1b3ea136ac97b04ddda
Matt Newbigging <matt.newbigging@aize.io>
2022-05-20 09:50:50 +0100

    adding config files

---
+++ src/Index.tsx
@@ -0,0 +1,15 @@
+import './index.scss';
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import { App } from './App';
+import { AppState } from './state/AppState';
+
+const appState = new AppState();
+
+const root = createRoot(document.getElementById('ui-root'));
+
+root.render(<App appState={appState} />);
+
+appState.loadGame();
