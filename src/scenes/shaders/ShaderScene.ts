9683bc813095ae73128c20cf47070f2a9719a978
MNewbigging <mrnewbigging@gmail.com>
2023-03-18 17:16:39 +0000

    sea shader

--- src/scenes/shaders/ShaderScene.ts
+++ src/scenes/shaders/ShaderScene.ts
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import * as dat from 'dat.gui';
 import { Camera, RawShaderMaterial } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
@@ -16,6 +17,11 @@ export class ShaderScene extends BaseScene {
   private controls: OrbitControls;
   private meshes: THREE.Mesh[] = [];
   private planeMat: THREE.RawShaderMaterial;
+  private gui: dat.GUI = new dat.GUI();
+  private debugObject = {
+    depthColor: '#186691',
+    surfaceColor: '#9bd8ff',
+  };
 
   public get camera(): Camera {
     return this._camera;
@@ -26,36 +32,109 @@ export class ShaderScene extends BaseScene {
     //this.scene.background = new THREE.Color('#1680AF');
 
     // Setup camera
-    const camera = new THREE.PerspectiveCamera(
-      75,
-      this.canvasListener.width / this.canvasListener.height,
-      0.1,
-      1000
-    );
-    camera.position.x = 0;
-    camera.position.y = 2;
-    camera.position.z = 2;
-    this._camera = camera;
-    this.controls = new OrbitControls(this.camera, this.canvasListener.canvas);
-    this.controls.enableDamping = true;
+    this.setupCamera();
 
     // Objects
-    const planeGeom = new THREE.PlaneBufferGeometry(2, 2, 128, 128);
+    const planeGeom = new THREE.PlaneBufferGeometry(2, 2, 512, 512);
     const planeMat = new THREE.ShaderMaterial({
       vertexShader: seaVertex,
       fragmentShader: seaFragment,
       side: THREE.DoubleSide,
       uniforms: {
         uTime: { value: 0.0 },
-        uBigWavesElevation: { value: 0.2 },
-        uBigWavesFrequency: { value: new THREE.Vector2(4, 1.5) },
+
+        uBigWavesElevation: { value: 0.1 },
+        uBigWavesFrequency: { value: new THREE.Vector2(2, 4) },
         uBigWavesSpeed: { value: 0.4 },
+
+        uSmallWavesElevation: { value: 0.1 },
+        uSmallWavesFrequency: { value: 2.5 },
+        uSmallWavesSpeed: { value: 0.2 },
+        uSmallWavesIterations: { value: 2.0 },
+
+        uDepthColor: { value: new THREE.Color(this.debugObject.depthColor) },
+        uSurfaceColor: { value: new THREE.Color(this.debugObject.surfaceColor) },
+        uColorOffset: { value: 0.08 },
+        uColorMultiplier: { value: 5.0 },
       },
     });
     this.planeMat = planeMat;
 
     const plane = new THREE.Mesh(planeGeom, planeMat);
     plane.rotation.x = -Math.PI * 0.5;
+
+    this.gui
+      .add(planeMat.uniforms.uBigWavesElevation, 'value')
+      .min(0)
+      .max(1)
+      .step(0.001)
+      .name('uBigWavesElevation');
+    this.gui
+      .add(planeMat.uniforms.uBigWavesFrequency.value, 'x')
+      .min(0)
+      .max(10)
+      .step(0.001)
+      .name('uBigWavesFrequencyX');
+    this.gui
+      .add(planeMat.uniforms.uBigWavesFrequency.value, 'y')
+      .min(0)
+      .max(30)
+      .step(0.001)
+      .name('uBigWavesFrequencyY');
+    this.gui
+      .add(planeMat.uniforms.uBigWavesSpeed, 'value')
+      .min(0)
+      .max(4)
+      .step(0.001)
+      .name('uBigWavesSpeed');
+
+    this.gui
+      .add(planeMat.uniforms.uSmallWavesElevation, 'value')
+      .min(0)
+      .max(1)
+      .step(0.001)
+      .name('uSmallWavesElevation');
+    this.gui
+      .add(planeMat.uniforms.uSmallWavesFrequency, 'value')
+      .min(0)
+      .max(30)
+      .step(0.001)
+      .name('uSmallWavesFrequency');
+    this.gui
+      .add(planeMat.uniforms.uSmallWavesSpeed, 'value')
+      .min(0)
+      .max(5)
+      .step(0.001)
+      .name('uSmallWavesSpeed');
+    this.gui
+      .add(planeMat.uniforms.uSmallWavesIterations, 'value')
+      .min(0)
+      .max(5)
+      .step(1)
+      .name('uSmallWavesIterations');
+
+    this.gui
+      .addColor(this.debugObject, 'depthColor')
+      .name('depthColor')
+      .onChange(() => this.planeMat.uniforms.uDepthColor.value.set(this.debugObject.depthColor));
+    this.gui
+      .addColor(this.debugObject, 'surfaceColor')
+      .name('surfaceColor')
+      .onChange(() =>
+        this.planeMat.uniforms.uSurfaceColor.value.set(this.debugObject.surfaceColor)
+      );
+    this.gui
+      .add(planeMat.uniforms.uColorOffset, 'value')
+      .min(0)
+      .max(1)
+      .step(0.001)
+      .name('uColorOffset');
+    this.gui
+      .add(planeMat.uniforms.uColorMultiplier, 'value')
+      .min(0)
+      .max(10)
+      .step(0.001)
+      .name('uColorMultiplier');
 
     this.meshes.push(plane);
     this.scene.add(plane);
@@ -72,6 +151,21 @@ export class ShaderScene extends BaseScene {
       m.geometry.dispose();
       (m.material as RawShaderMaterial).dispose();
     });
+  }
+
+  private setupCamera() {
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      this.canvasListener.width / this.canvasListener.height,
+      0.1,
+      1000
+    );
+    camera.position.x = 0;
+    camera.position.y = 2;
+    camera.position.z = 2;
+    this._camera = camera;
+    this.controls = new OrbitControls(this.camera, this.canvasListener.canvas);
+    this.controls.enableDamping = true;
   }
 
   private setupFlag() {
